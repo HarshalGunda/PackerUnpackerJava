139d3a88a98ae3368006cca6d4e62a66e8b5fa14
GUNDA-PC <GUNDA-PC@DESKTOP-K2UV3VN>
2021-01-08 02:38:38 +0530

    created 3 files as main, template, packer in java

---
+++ main.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+
---
+++ template.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicBorders;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
